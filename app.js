f0bd3ab3fe5d8a0c1e1b4ee3f83922b5d03c949e
Mohammed Hamdoune <sidlynx@gmail.com>
2017-07-14 17:46:23 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,49 +1,22 @@
 var App = {};
 App.data = {};
 App.videos = [];
+App.transformedVideos = [];
+App.searchedVideos = [];
 App.page = 1;
+App.totalPages = 0;
 App.render = function () {
-    //
     App.videos = App.data.data;
     var search = $("#search").val();
     var itemsToTake = parseInt($('input[name=items]:checked').val());
     App.optVideos = [];
-    App.videos.forEach(function (video) {
-        var optVideo = {};
-        optVideo.id = video.resource_key;
-        if (video.user.pictures) {
-            optVideo.user_image_url = video.user.pictures.sizes[video.user.pictures.sizes.length - 1].link;
-        }
-        optVideo.user_name = video.user.name;
-        optVideo.user_link = video.user.link;
-
-        optVideo.description = video.description;
-
-        if(optVideo.description){
-            optVideo.short_description = optVideo.description.substring(0,20);
-        }
-
-        optVideo.name = video.name;
-        optVideo.link = video.link;
-
-        if (video.pictures) {
-            optVideo.image_url = video.pictures.sizes[video.pictures.sizes.length - 1].link;
-        }
-
-        optVideo.comments_count = video.metadata.connections.comments.total;
-        optVideo.likes_count = video.metadata.connections.likes.total;
 
 
-        if(optVideo.description && optVideo.description.indexOf(search) >= 0){
-            App.optVideos.push(optVideo);
-        }
-
-        App.optVideos = _.take(App.optVideos,itemsToTake);
-    })
+    App.optVideos = _.take(App.optVideos, itemsToTake);
 
 
     var template = $('#template').html();
-    Mustache.parse(template);   // optional, speeds up future uses
+    Mustache.parse(template);
     var rendered = Mustache.render(template, App);
     $('#target').html(rendered);
 
@@ -64,6 +37,56 @@ App.loadVideos = function () {
 }
 
 
+App.transformVideos = function (videos) {
+    var tempVideos = [];
+    videos.forEach(function (video) {
+        var optVideo = {};
+        optVideo.id = video.resource_key;
+        if (video.user.pictures) {
+            optVideo.user_image_url = video.user.pictures.sizes[video.user.pictures.sizes.length - 1].link;
+        }
+        optVideo.user_name = video.user.name;
+        optVideo.user_link = video.user.link;
+
+        optVideo.description = video.description;
+
+        if (optVideo.description) {
+            optVideo.short_description = optVideo.description.substring(0, 20);
+        }
+
+        optVideo.name = video.name;
+        optVideo.link = video.link;
+
+        if (video.pictures) {
+            optVideo.image_url = video.pictures.sizes[video.pictures.sizes.length - 1].link;
+        }
+
+        optVideo.comments_count = video.metadata.connections.comments.total;
+        optVideo.likes_count = video.metadata.connections.likes.total;
+
+        tempVideos.push(optVideo);
+
+        return tempVideos;
+    })
+}
+
+App.searchVideos = function (videos, search) {
+    var tempVideos = [];
+    videos.forEach(function (video) {
+        if (video.description && video.description.indexOf(search) >= 0) {
+            tempVideos.push(video);
+        }
+    })
+    return tempVideos;
+}
+
+App.next = function(videos,page){
+
+}
+App.previous = function(){
+
+}
+
 $(document).ready(function () {
     App.loadVideos();
 
@@ -71,11 +94,19 @@ $(document).ready(function () {
         App.render();
     });
 
-    $("#search").change(function() {
+    $("#search").change(function () {
         App.render();
     });
 
-    $("#search").keyup(function() {
+    $("#search").keyup(function () {
         App.render();
+    });
+
+    $("#prev").click(function () {
+        App.previous();
+    });
+
+    $("#next").click(function () {
+        App.next();
     });
 });
